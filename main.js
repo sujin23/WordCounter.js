cae5b934c505625070472b2deb3fd97c33448b79
sujin23 <sujin@swu.ac.kr>
2022-07-18 22:23:25 +0900

    반복문을 이용한 배열 순회

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var entireText = document.getElementsByTagName('body')[0].innerText;
 var splitedText = entireText.split(' ');
 
-splitedText
+for(var i = 0; i < splitedText.length; i++){
+	console.log(splitedText[i])
+}
