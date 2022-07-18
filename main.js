07309915b30105db77aee9cbf30f11a9b49d6854
sujin23 <sujin@swu.ac.kr>
2022-07-18 23:01:45 +0900

    배열 정렬하기 (main 코드 추가 X)

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// 1) 현재 웹페이지의
-
-// 2) 모든 텍스트에서
 var entireText = document.getElementsByTagName('body')[0].innerText;
 
-// 3) 단어들을 쪼갠 후에
 var splitedText = entireText.split(' ');
 
-// 4) 등장 횟수를 계산하고
 var countedWord = {};
 for(var i = 0; i < splitedText.length; i++){
 	var word = splitedText[i];
@@ -16,3 +11,4 @@ for(var i = 0; i < splitedText.length; i++){
 		countedWord[word] = countedWord[word] + 1;
 	}
 }
+
