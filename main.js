c843ca87ab4567ecca6ab15a122448b1f205899d
sujin23 <sujin@swu.ac.kr>
2022-07-18 23:41:25 +0900

    콘솔 출력과 대소문자 구분 없애기

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var splitedText = entireText.split(' ');
 
 var countedWord = {};
 for(var i = 0; i < splitedText.length; i++){
-	var word = splitedText[i];
+	var word = splitedText[i].toLowerCase();
 	if(countedWord[word] == undefined) {
 		countedWord[word] = 1;
 	} else {
@@ -19,3 +19,7 @@ for(var name in countedWord) {
 countedWordArr.sort(function(a, b){
 	return a[1] - b[1];
 })
+
+for(var i = 0; i < countedWordArr.length; i++){
+    console.log(countedWordArr[i][0], countedWordArr[i][1])
+}
